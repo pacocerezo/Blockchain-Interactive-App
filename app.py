acda7dc13c62fcb3ddb93e44f4dc86d886dc36a0
pacocerezo <pacoceval@gmail.com>
2023-02-17 13:14:29 +0100

    Added Secure attribute to cookies

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
-    response.headers["SameSite"] = "None; Secure"
+    response.headers["SameSite"] = "None"
+    response.headers["Secure"] = True
     return response
 
 
